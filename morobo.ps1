e79c8f72961d267afd320664daeec291ba0181cf
Atla <30910804+Atla-Dev@users.noreply.github.com>
2026-04-13 19:21:38 +0100

    Initial Commit

---
+++ morobo.ps1
@@ -0,0 +1,15 @@
+#===============================================================#
+# Script: Robocopy Multiple Sources                             #
+# Written: Atla-Dev                                             #
+# Description: This script copies multiple files via robocopy   #
+#===============================================================#
+
+# Put the folders you want to copy here
+$sources = "C:\folder1","C:\folder2","C:\folder3"
+$destination = "C:\destinationfolder"
+
+
+# Change the switches to suit your preference /S copies all directories but ignores empty ones /R:5 retries 5 times /W:5 waits 5 seconds between retries /Z restartable after a network issue
+foreach( $src in $sources ) {
+    robocopy $src $destination /S /R:5 /W:5 /Z /LOG:"$destination/morobolog.txt" /TEE
+}
