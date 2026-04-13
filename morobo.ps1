5b0b63805b7189f9847e4c2430da2e3b522ab901
Atla <30910804+Atla-Dev@users.noreply.github.com>
2026-04-13 19:24:34 +0100

    added: comment for network device support

--- morobo.ps1
+++ morobo.ps1
@@ -4,9 +4,10 @@
 # Description: This script copies multiple files via robocopy   #
 #===============================================================#
 
-# Put the folders you want to copy here
+# Put the folders you want to copy here, if device is on the network use \\assetnumber\path\to\destinationfolder\
 $sources = "C:\folder1","C:\folder2","C:\folder3"
 $destination = "C:\destinationfolder"
+
 
 
 # Change the switches to suit your preference /S copies all directories but ignores empty ones /R:5 retries 5 times /W:5 waits 5 seconds between retries /Z restartable after a network issue
